e0eed1d77d9f67e5fe1b7e200d3bf38196834eb5
joan <joan.asselot@gmail.com>
2024-09-04 21:39:12 +0200

    fix le get model summary

--- api/main.py
+++ api/main.py
@@ -1,60 +1,5 @@
-# from fastapi import FastAPI, UploadFile, File, HTTPException
-# from pydantic import BaseModel
-# from models_api.modelsClass import VGG16Model
-# import shutil
-# import os
-
-
-# CURRENT_DIR = os.path.dirname(__file__)
-# GOLD_MODEL_FILE = "./pretrain_models/gold_vgg16.h5"
-# GOLD_LABELENCODER_FILE = "./pretrain_models/encoder.joblib"
-# GOLD_MODEL_PATH = os.path.join(CURRENT_DIR, GOLD_MODEL_FILE)
-# GOLD_LABELENCODER_PATH = os.path.join(CURRENT_DIR, GOLD_LABELENCODER_FILE)
-
-# # GOLD_MODEL_PATH = "./gold_vgg16.h5"
-# # GOLD_LABELENCODER_PATH = "./pretrain_models/encoder.joblib"
-
-# print(f"#################  Directory : {GOLD_MODEL_PATH}")
-
-# GOLD_MODEL_PATH = GOLD_MODEL_PATH.replace("\\", "/")
-# app = FastAPI()
-
-# # Initialize the model
-# model = VGG16Model.from_pretrained(GOLD_MODEL_PATH)
-# model.load_encoder(GOLD_LABELENCODER_PATH)
-
-
-# # @app.post("/predict", response_model=PredictionResponse)
-# # async def predict_image(file: UploadFile = File(...)):
-# #     try:
-# #         # Save the uploaded file
-# #         file_location = f"temp/{file.filename}"
-# #         with open(file_location, "wb") as buffer:
-# #             shutil.copyfileobj(file.file, buffer)
-
-# #         # Make prediction
-# #         prediction = model.predict(file_location)
-# #         pred_class= model.predict_class(file_location)
-
-# #         # Clean up the saved file
-# #         os.remove(file_location)
-
-# #         return pred_class
-# #         )
-# #     except Exception as e:
-# #         raise HTTPException(status_code=500, detail=str(e))
-    
-# @app.get("/model-summary")
-# async def get_model_summary():
-#     try:
-#         summary = model.summary()
-#         return {"summary": summary}
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-    
-
-
-
+import contextlib
+import io
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
 from models_api.modelsClass import VGG16Model
@@ -67,12 +12,12 @@ GOLD_LABELENCODER_FILE = "pretrain_models/encoder.joblib"
 GOLD_MODEL_PATH = os.path.join(CURRENT_DIR, GOLD_MODEL_FILE)
 GOLD_LABELENCODER_PATH = os.path.join(CURRENT_DIR, GOLD_LABELENCODER_FILE)
 
-# Convert paths to use forward slashes
+# Converti les backslashes en slashes
 GOLD_MODEL_PATH = GOLD_MODEL_PATH.replace("\\", "/")
 GOLD_LABELENCODER_PATH = GOLD_LABELENCODER_PATH.replace("\\", "/")
 
 
-# Check if files exist
+# Verifie lexistence des fichiers
 if not os.path.exists(GOLD_MODEL_PATH):
     print(f"Error: Model file does not exist at {GOLD_MODEL_PATH}")
 if not os.path.exists(GOLD_LABELENCODER_PATH):
@@ -81,7 +26,7 @@ if not os.path.exists(GOLD_LABELENCODER_PATH):
 app = FastAPI()
 
 
-# Initialize the model
+# Initialization du model
 try:
     model = VGG16Model.from_pretrained(GOLD_MODEL_PATH)
     model.load_encoder(GOLD_LABELENCODER_PATH)
@@ -95,8 +40,12 @@ class PredictionResponse(BaseModel):
 @app.get("/model-summary")
 async def get_model_summary():
     try:
-        summary = model.summary()
-        return {"summary": summary}
+                # Capture the model summary
+        summary_io = io.StringIO()
+        with contextlib.redirect_stdout(summary_io):
+            model.summary()
+        summary_str = summary_io.getvalue()
+        return {"summary": summary_str}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
@@ -104,21 +53,19 @@ async def get_model_summary():
 @app.post("/predict", response_model=PredictionResponse)
 async def predict_image(file: UploadFile = File(...)):
     try:
-        # Ensure the temp directory exists
+        # Check existence du repertoire temp
         temp_dir = "temp"
         if not os.path.exists(temp_dir):
             os.makedirs(temp_dir)
 
-        # Save the uploaded file
+        # Sauvegarde temporaire du fichier
         file_location = os.path.join(temp_dir, file.filename)
         with open(file_location, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
 
-        # Make prediction
-        prediction = model.predict(file_location)
         pred_class= model.predict_class(file_location)
 
-        # Clean up the saved file
+        # Clean up
         os.remove(file_location)
 
         return  PredictionResponse(predicted_class=str(pred_class))
