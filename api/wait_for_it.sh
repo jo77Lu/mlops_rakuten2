79228dfc8ef69028b57a3114aa8a3efd2ea9378f
indira893 <indira.massoassiem@yahoo.fr>
2024-09-09 09:45:16 +0200

    Github actions

--- api/wait_for_it.sh
+++ api/wait_for_it.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 # wait-for-it.sh: Waits until a given host and port are ready.
 
-set -e
-
-host="$1"
-shift
-port="$1"
-shift
+host=$1
 cmd="$@"
+timeout=600
+start_time=$(date+%s)
 
-until nc -z "$host" "$port"; do
-  >&2 echo "Waiting for $host:$port to be available..."
-  sleep 1
+until $(curl --output /dev/null --silent --head --fail http://$host); do
+  printf '.'
+
+  current_time=$(date+%s)
+  elapsed_time=$((current_time - start_time))
+
+  if [ $elapsed_time -ge $timeout ]; then
+    echo "Timeout reached after $timeout seconds. Exiting."
+    exit 1
+  sleep 5
 done
 
->&2 echo "$host:$port is available - running command"
 exec $cmd
